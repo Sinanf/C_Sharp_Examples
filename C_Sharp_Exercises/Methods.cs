9d66440d6080d0864bc862c6dda7cf982643e260
Sinanf <sinanfozdemir@gmail.com>
2022-04-20 21:30:47 +0300

    Methods Exercises
    Class created for exercises

---
+++ C_Sharp_Exercises/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Exercises
+{
+    internal class Methods
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             */
+
+        }
+    }
+}
